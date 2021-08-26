4bbc224d7a24a3fd6f6586dc0da0afecf92c10ba
Владислав Федоров <vfedorov@ad.local>
2021-08-26 09:13:09 +0300

    2nd and 3rd header in progress

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,17 @@
-console.log(1);
+const btn = document.querySelector('#btn');
+const menuNotLogged = document.querySelector('.menu-type-notlogged');
+const buttonsContainer = document.querySelector('.entrance-buttons-container')
+
+btn.onclick = function (e) {
+  e.preventDefault();
+  const firstHeader = document.querySelector('#choice1');
+  const secondHeader = document.querySelector('#choice2');
+  if (firstHeader.checked){
+    menuNotLogged.style.display = 'block';
+    buttonsContainer.style.display = 'block';
+  }
+  else {
+    menuNotLogged.style.display = 'none';
+    buttonsContainer.style.display = 'none';
+    }
+};
